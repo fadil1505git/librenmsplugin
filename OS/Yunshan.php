b82c6c6e4d25b118e50ee4d744fbae808aeaa2bd
fadil1505git <156063868+fadil1505git@users.noreply.github.com>
2026-07-29 19:06:08 +0800

    Create Yunshan.php for Huawei (Vrp) OS support
    Add Yunshan OS class that inherits from Vrp

---
+++ OS/Yunshan.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace LibreNMS\OS;
+
+class Yunshan extends Vrp
+{
+    // Sistem YunShan OS akan mewarisi (inherit) 100% fungsi daripada Vrp.php
+    // Termasuklah fungsi 'discoverTransceivers' yang telah dibaiki ejaan VenderSn-nya.
+}
